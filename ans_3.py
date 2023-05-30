d16c2eb1e1df6beab4480c7016605f37cf919d12
Rhythm Rawat <106919276+Rhythm1821@users.noreply.github.com>
2023-05-30 12:07:48 +0530

    Update ans_3.py

--- ans_3.py
+++ ans_3.py
@@ -1,3 +1,9 @@
+"""
+💡 **Q3.** Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
+
 def searchInsert(nums, target):
     left = 0
     right = len(nums) - 1
